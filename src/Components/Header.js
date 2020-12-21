ba20d9f58d5d0f39e3e03487f67983f4bba6662f
Kevin <knguyen3415@gmail.com>
2020-12-21 15:26:06 -0800

    Setup Header component

---
+++ src/Components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../Stylesheets/Header.css';
+import { Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/Help';
+
+function Header() {
+    return(
+        <div className="header">
+            <div className="header__left">
+                <Avatar 
+                    className="header__avatar"
+                    alt="Kevin Nguyen"
+                    src=""
+                />
+
+                <AccessTimeIcon />
+            </div>
+            <div className="header__search">
+                <SearchIcon />
+
+                <input placeholder="Search Clever Programer"/>
+            </div>
+            <div className="header__right">
+                <HelpOutlineIcon />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
